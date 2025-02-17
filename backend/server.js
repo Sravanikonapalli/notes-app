dc390312a7ec97dfe4b35867c2c0ed9c6aa992b4
Sravanikonapalli <konapallisravani@gmail.com>
2025-02-17 21:29:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const initializeDbAndServer = async () => {
             );
         `);
 
-        app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
+    app.listen(PORT, '0.0.0.0', () => console.log(`Server running at http://0.0.0.0:${PORT}/`));
     } catch (error) {
         console.error("DB Error:", error.message);
         process.exit(1);
